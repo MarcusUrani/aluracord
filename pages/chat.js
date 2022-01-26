cda1555a4b4f467a1da84c2e45717cb7df71d8a2
Marcus Urani <marcus.urani20@gmail.com>
2022-01-26 10:30:22 -0300

    Adicionando a página de chat e iniciando a lógica

--- pages/chat.js
+++ pages/chat.js
@@ -1,9 +1,163 @@
-const PaginaChat = () => {
+import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import React from "react";
+import appConfig from "../config.json";
+import Header from "../Components/Header";
+import { useState } from "react/cjs/react.development";
+
+const ChatPage = () => {
+  const [mensagem, setMensagem] = useState("");
+  const [messageArray, setMessageArray] = useState({});
+  //Usuário digita no campo TextArea
+  //Aperta ENTER para enviar
+  //Adiciona a mensagem a lista de mensagens
+  //Exibe a mensagem
+
+  /*
+  [X] Campo criado
+  [] Utilizar OnChange para pegar o valor da mensagem(usar if caso Enter seja clicado)
+  [] Lista de mensagens
+   */
+
   return (
-    <div>
-      <h1>Seja bem vindo ao chat!</h1>
-    </div>
+    <Box
+      styleSheet={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://i1.wp.com/nerdizmo.uai.com.br/wp-content/uploads/sites/29/2021/05/wallpapers-do-star-wars-17.jpg?ssl=1)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: "100%",
+          maxWidth: "95%",
+          maxHeight: "95vh",
+          padding: "32px",
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: "relative",
+            display: "flex",
+            flex: 1,
+            height: "80%",
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px",
+          }}
+        >
+          <MessageList />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              value={mensagem}
+              onChange={(event) => {
+                const valor = event.target.value;
+                setMensagem(valor);
+              }}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  let valor = event.target.value;
+                  setMessageArray(valor);
+                  console.log(messageArray);
+                  setMensagem("");
+                }
+              }}
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: "12px",
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
-export default PaginaChat;
+const MessageList = (props) => {
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "scroll",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: "16px",
+      }}
+    >
+      <Text
+        // key={mensagem.id}
+        tag="li"
+        styleSheet={{
+          borderRadius: "5px",
+          padding: "6px",
+          marginBottom: "12px",
+          hover: {
+            backgroundColor: appConfig.theme.colors.neutrals[700],
+          },
+        }}
+      >
+        <Box
+          styleSheet={{
+            marginBottom: "8px",
+          }}
+        >
+          <Image
+            styleSheet={{
+              width: "20px",
+              height: "20px",
+              borderRadius: "50%",
+              display: "inline-block",
+              marginRight: "8px",
+            }}
+            src={`https://github.com/vanessametonini.png`}
+          />
+          {/* <Text tag="strong">{mensagem.de}</Text> */}
+          <Text
+            styleSheet={{
+              fontSize: "10px",
+              marginLeft: "8px",
+              color: appConfig.theme.colors.neutrals[300],
+            }}
+            tag="span"
+          >
+            {new Date().toLocaleDateString()}
+          </Text>
+        </Box>
+        {/* {mensagem.texto} */}
+      </Text>
+    </Box>
+  );
+};
+
+export default ChatPage;
